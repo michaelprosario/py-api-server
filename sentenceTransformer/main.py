217f1e30ba700eec814cb0db317daf1f47814935
michaelprosario <michael.p.rosario@gmail.com>
2024-01-28 10:48:30 +0000

    feat ( sentence transformers ) remove

--- sentenceTransformer/main.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer
-from sentence_transformers.util import cos_sim
-
-sentences = ['Lets have a drink', 'Do you want to go for a drink?']
-
-model = SentenceTransformer('thenlper/gte-small')
-embeddings = model.encode(sentences)
-print(cos_sim(embeddings[0], embeddings[1]))
--- sentenceTransformer/notes.md
+++
@@ -1,7 +0,0 @@
-## Install
-
-```
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
-pip install transformers[torch]
-pip install -U sentence-transformers
-```
